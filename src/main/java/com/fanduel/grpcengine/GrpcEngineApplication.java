8a3234f9aa90bd16e6523abcb635040d3a336d21
Marin Petcu <marin.petcu@fanduel.com>
2024-03-18 15:21:52 +0200

    setup

---
+++ src/main/java/com/fanduel/grpcengine/GrpcEngineApplication.java
@@ -0,0 +1,13 @@
+package com.fanduel.grpcengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fanduel/grpcengine/GrpcEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanduel.grpcengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
