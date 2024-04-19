f82f814c17ef2ebb06c5e2a3b2d5816c3fe1e64b
Marin Petcu <marin.petcu@fanduel.com>
2024-04-19 10:58:00 +0300

    Remove Spring controller

--- src/main/java/com/fanduel/grpcengine/service/ContributeService.java
+++ src/main/java/com/fanduel/grpcengine/service/ContributeService.java
@@ -1,12 +1,12 @@
 package com.fanduel.grpcengine.service;
 
-import com.fanduel.marin.contrib.ContributeRequest;
-import com.fanduel.marin.contrib.ContributeResponse;
-import com.fanduel.marin.contrib.ContributionServiceGrpc;
+import beyondplay.contrib.v1.ContributionServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static beyondplay.contrib.v1.Contribute.*;
 
 @GrpcService
 public class ContributeService extends ContributionServiceGrpc.ContributionServiceImplBase {
